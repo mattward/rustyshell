5cf05139cb6d417b5c1391b14685e112c5d4703d
Matt Ward <matt@wardm.uk>
2021-06-20 21:59:25 +0100

    Add *. command to list history

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ const PROG_NAME: &str = "rustyshell";
 const PROG_VER: &str = "0.0.1";
 
 fn is_builtin(command: &str) -> bool {
-    return command.eq("!!");
+    command.eq("!!") || command.starts_with('*')
 }
 
 fn execute_command(command: &str, args: &[&str]) {
@@ -18,35 +18,45 @@ fn execute_command(command: &str, args: &[&str]) {
     child.wait().expect("Command failed");
 }
 
-fn execute_line(line: &str) {
+fn execute_builtin(command: &str, args: &[&str], history: &Vec<String>) {
+    if (command.eq("*.")) {
+        list_history(&history);
+    }
+}
+
+fn execute_line(line: &str, history: &Vec<String>) {
     let tokens: Vec<&str> = line.split_whitespace().collect();
     if tokens.len() == 0 {
         return;
     }
-    let command = tokens[0];
-    println!("Command: {}", command);
 
+    let command = tokens[0];
     let args: &[&str] = &tokens[1..];
-    if tokens.len() > 1 {
-        println!("Args: {}", args.join(", "));
-    }
 
     if is_builtin(&command) {
-        println!("Unimplemented, but would execute the built-in '{}'", &command);
-    }
-    else {
+        execute_builtin(&command, args, &history);
+    } else {
         execute_command(&command, args);
     }
 }
 
 fn make_prompt() -> String {
-    return PROG_NAME.to_owned() + "::> ";
+    PROG_NAME.to_owned() + "::> "
+}
+
+fn list_history(history: &Vec<String>) {
+    let mut i = 1;
+    for line in history.iter() {
+        println!("{}. {}", i, line);
+        i += 1;
+    }
 }
 
 fn main() {
-    println!("{}, v{}", PROG_NAME, PROG_VER);
+    println!("** {}, v{} **", PROG_NAME, PROG_VER);
 
     let prompt = make_prompt();
+    let mut history: Vec<String> = vec![];
 
     loop {
         print!("{}", prompt);
@@ -54,11 +64,14 @@ fn main() {
         let mut command_line = String::new();
         io::stdin().read_line(&mut command_line).expect("Unable to read from stdin");
 
-        if command_line.trim().eq("exit") {
+        let command_line = command_line.trim().to_string();
+
+        if command_line.eq("exit") {
             break;
+        } else {
+            execute_line(&command_line, &history);
         }
-        else {
-            execute_line(&command_line);
-        }
+
+        history.push(command_line);
     }
 }
